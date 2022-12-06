ef4f7e99f1a0c45282e0f63a0afd284e003e52a9
Honghyun Ahn <hyunynim1@naver.com>
2022-12-06 18:20:03 +0900

    [DEV] Add addInfo function

--- myTestPage/views.py
+++ myTestPage/views.py
@@ -1,9 +1,38 @@
 from django.shortcuts import render
 
+import sqlite3
+
+def addInfo(_name, _age):
+    con = sqlite3.connect('db.sqlite3')
+    cur = con.cursor()
+
+    cur.execute('SELECT COUNT(*) FROM myTestPage_mytestpagedb')
+    numberOfRows = cur.fetchone()
+    id = int(numberOfRows[0])
+    id += 1
+
+    cur.execute('INSERT INTO myTestPage_mytestpagedb VALUES (?, ?, ?)', (id, _name, _age))
+
+    con.commit()
+    con.close()
+
+
 def IndexPage(_request):
     return render(_request, 'myTestPage/index.html')
 
 def UploadInfo(_request):
     name = _request.POST.get('name')
     age = _request.POST.get('age')
-    return render(_request, 'myTestPage/index.html')
+
+    resultMessage = ''
+    if not(1 < len(name) and len(name) <= 10):
+        resultMessage = 'Length of name should be greater than 1 and less than or equal to 10'
+    
+    elif(age.isdigit() == False):
+        resultMessage = 'Age should be a number'
+    
+    else:
+        resultMessage = 'Hello ' + name + '(' + age + '). Welcome to TestPage'
+        addInfo(name, age)
+
+    return render(_request, 'myTestPage/result.html', {'result':resultMessage})
